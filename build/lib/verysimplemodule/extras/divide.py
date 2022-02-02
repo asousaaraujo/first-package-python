f86322ce9985767a7c06ddf0a317e72262f0a3a6
André Araújo <andre.araujo@kroger.com>
2022-02-02 10:45:02 -0300

    first commit

---
+++ build/lib/verysimplemodule/extras/divide.py
@@ -0,0 +1,2 @@
+def divide(x: int, y: int) -> int:
+    return x // y
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+setup(
+        name="verysimplemodule", 
+        version=VERSION,
+        author="André Araújo",
+        author_email="dedeco@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
